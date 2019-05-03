392ac37d16cdab4de77819d048935f5f7cf6bbde
Amol <amol.digiinterface@gmail.com>
2019-05-03 20:17:53 +0530

    Splash screeen created

---
+++ app/src/main/java/com/example/shianikha/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.shianikha;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.shianikha.activities.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        /*new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                *//* Create an Intent that will start the Menu-Activity. *//*
+                Intent mainIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+        Intent i=new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(i);*/
+
+    }
+}
