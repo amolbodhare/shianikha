d18496bd55a4dc006a5afd9e2835e813a1e44be9
vijendrajs <vijendrajs@gmail.com>
2019-08-09 20:37:24 +0530

    "09-08-19 08.00 PM" s

--- app/src/main/java/com/example/shianikha/activities/MyActivity.java
+++ app/src/main/java/com/example/shianikha/activities/MyActivity.java
@@ -38,6 +38,7 @@ public class MyActivity extends AppCompatActivity implements View.OnClickListene
     private View fragMentView;
     Context context;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
