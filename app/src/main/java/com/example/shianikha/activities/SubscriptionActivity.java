66628ab26bf80f74e1128e091ec2490f0890ec03
user <rohit.digiinterface@gmail.com>
2019-08-09 07:33:38 -0700

    Design changes done in subscription and navigation implemented from Favourite fragment to Filter Activity

--- app/src/main/java/com/example/shianikha/activities/SubscriptionActivity.java
+++ app/src/main/java/com/example/shianikha/activities/SubscriptionActivity.java
@@ -38,41 +38,6 @@ public class SubscriptionActivity extends AppCompatActivity implements View.OnCl
 
         subPlanSliderAdapter = new SubscriptionSliderAdapter(context);
 
-        subPlanSlideViewPager.setPageTransformer(false, new ViewPager.PageTransformer() {
-            @Override
-            public void transformPage(View page, float position) {
-
-                page.setCameraDistance(20000);
-
-
-                if (position < -1){
-                    page.setAlpha(0);
-                }
-                else if (position <= 0)
-                {
-                    page.setAlpha(1);
-                    page.setPivotX(page.getWidth());
-                    page.setRotationY(90*Math.abs(position));
-                }
-                else if (position <= 1){
-                    page.setAlpha(1);
-                    page.setPivotX(0);
-                    page.setRotationY(-90*Math.abs(position));
-                }
-                else{
-                    page.setAlpha(0);
-                }
-
-                if (Math.abs(position) <= 0.5){
-                    page.setScaleY(Math.max(.4f,1-Math.abs(position)));
-                }
-                else if (Math.abs(position) <= 1){
-                    page.setScaleY(Math.max(.4f,1-Math.abs(position)));
-
-                }
-            }
-        });
-
         subPlanSlideViewPager.setAdapter(subPlanSliderAdapter);
 
     }
@@ -104,6 +69,5 @@ public class SubscriptionActivity extends AppCompatActivity implements View.OnCl
     private int getItem(int i) {
         return subPlanSlideViewPager.getCurrentItem() + i;
     }
-
 
 }
